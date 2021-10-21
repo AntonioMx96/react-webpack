ce2f871410d069b0d44316b89be1e5f55ddd2383
AntonioMx96 <antonio.060896@gmail.com>
2021-10-21 18:44:18 -0500

    configuracion inicial

---
+++ config/webpack.dev.js
@@ -0,0 +1,15 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require("path")
+
+/** @type {import('webpack').Configuration} */
+const devConfig = {
+    mode:"development",
+    devServer: {
+        port: 3000,
+        static: {
+          directory: path.join(__dirname, '../dist'),
+        },
+      },
+}
+module.exports = merge(common, devConfig);
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+
+function App() {
+  return (
+    < >
+      <h1>Hola que hace</h1>
+    </>
+  );
+}
+
+export default App;
